bbbf1e9f3a142f0545528263869fc212564d8adc
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 11:19:25 +0530

    Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -10,24 +10,21 @@ def get_soundex_code(c):
     }
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
-
 def generate_soundex(name):
     if not name:
         return ""
 
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
+    # Retain the first letter
+    soundex = [name[0].upper()]
+    prev_code = get_soundex_code(soundex[0])
 
     for char in name[1:]:
         code = get_soundex_code(char)
         if code != '0' and code != prev_code:
-            soundex += code
+            soundex.append(code)
             prev_code = code
-        if len(soundex) == 4:
+        if len(soundex) == 4:  # Stop once we have the first letter + 3 digits
             break
 
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-
-    return soundex
+    # Join the list into a string and pad with zeros if necessary
+    return ''.join(soundex).ljust(4, '0')
