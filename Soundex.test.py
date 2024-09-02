08f90bb59068990ebdecb0246be6d20bce840bd9
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 13:29:33 +0530

    Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,21 +9,19 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
-    def test_names_with_similar_sounds(self):
-        self.assertEqual(generate_soundex("Robert"), "R163")
-        self.assertEqual(generate_soundex("Rupert"), "R163")
-        self.assertEqual(generate_soundex("Rubin"), "R150")
-    def test_names_with_h_w(self):
-        self.assertEqual(generate_soundex("Hemming"), "H552")
-        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+    def test_multiple_characters(self):
+        self.assertEqual(generate_soundex("Smith"), "S530")
+        self.assertEqual(generate_soundex("Jefferson"), "J350")
+        self.assertEqual(generate_soundex("Washington"), "W250")
 
-    def test_various_names(self):
-        self.assertEqual(generate_soundex("Tymczak"), "T522")
-        self.assertEqual(generate_soundex("Pfister"), "P236")
+    def test_non_alphabetic_characters(self):
+        self.assertEqual(generate_soundex("123"), "1000")
+        self.assertEqual(generate_soundex("Hello, World!"), "H400")
 
-    def test_name_with_repeated_consonants(self):
-        self.assertEqual(generate_soundex("Jackson"), "J250")
-        self.assertEqual(generate_soundex("Tucker"), "T260")
+    def test_edge_cases(self):
+        self.assertEqual(generate_soundex("AAAA"), "A000")
+        self.assertEqual(generate_soundex("BBBB"), "B100")
+        self.assertEqual(generate_soundex("CCCC"), "C200")
 
 if __name__ == '__main__':
     unittest.main()
