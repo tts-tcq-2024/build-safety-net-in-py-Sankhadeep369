4420b61477b501d5e361bf83786ae1edcdf7ba5b
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 14:19:59 +0530

    Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -11,9 +11,6 @@ class TestSoundex(unittest.TestCase):
 
     def test_multiple_adjacent_duplicates(self):
         self.assertEqual(generate_soundex("hello"), "H400")
-
-    def test_duplicates_separated_by_vowel(self):
-        self.assertEqual(generate_soundex("example"), "E236")
     
 if __name__ == '__main__':
     unittest.main()
