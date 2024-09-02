76401f5518ee9ca5f929d0de091c3070de32e746
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 13:39:06 +0530

    Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,19 +9,18 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
-    def test_multiple_characters(self):
-        self.assertEqual(generate_soundex("Smith"), "S530")
-        self.assertEqual(generate_soundex("Jefferson"), "J350")
-        self.assertEqual(generate_soundex("Washington"), "W250")
+    def test_multiple_adjacent_duplicates(self):
+        self.assertEqual(generate_soundex("hello"), "H400")
 
-    def test_non_alphabetic_characters(self):
-        self.assertEqual(generate_soundex("123"), "1000")
-        self.assertEqual(generate_soundex("Hello, World!"), "H400")
+    def test_duplicates_separated_by_vowel(self):
+        self.assertEqual(generate_soundex("example"), "E236")
 
-    def test_edge_cases(self):
-        self.assertEqual(generate_soundex("AAAA"), "A000")
-        self.assertEqual(generate_soundex("BBBB"), "B100")
-        self.assertEqual(generate_soundex("CCCC"), "C200")
+    def test_duplicates_separated_by_consonant(self):
+        self.assertEqual(generate_soundex("winter"), "W543")
 
+    def test_long_name(self):
+        self.assertEqual(generate_soundex("abcdefghijklmnopqrstuvwxyz"), "A23456")
+
+    
 if __name__ == '__main__':
     unittest.main()
