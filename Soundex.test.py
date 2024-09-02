10565c1665727ebca1bd31812a2d9c84fe97230e
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 13:08:07 +0530

    Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -14,14 +14,6 @@ class TestSoundex(unittest.TestCase):
         self.assertEqual(generate_soundex("Rupert"), "R163")
         self.assertEqual(generate_soundex("Rubin"), "R150")
 
-    def test_names_with_h_w(self):
-        self.assertEqual(generate_soundex("Hemming"), "H552")
-        self.assertEqual(generate_soundex("Ashcraft"), "A261")
-
-    def test_various_names(self):
-        self.assertEqual(generate_soundex("Tymczak"), "T522")
-        self.assertEqual(generate_soundex("Pfister"), "P236")
-
     def test_name_with_repeated_consonants(self):
         self.assertEqual(generate_soundex("Jackson"), "J250")
         self.assertEqual(generate_soundex("Tucker"), "T260")
