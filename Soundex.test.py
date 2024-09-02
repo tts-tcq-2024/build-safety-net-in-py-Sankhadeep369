c0a50c33516d1ea594cfd005d615b101732c35eb
Sankh <72002970+Sankhadeep369@users.noreply.github.com>
2024-09-02 11:19:52 +0530

    Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -8,7 +8,29 @@ class TestSoundex(unittest.TestCase):
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
+        self.assertEqual(generate_soundex("B"), "B000")
 
-    
+    def test_same_sound_code(self):
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
+
+    def test_different_first_letter(self):
+        self.assertEqual(generate_soundex("Robert"), "R163")
+        self.assertEqual(generate_soundex("Rupert"), "R163")
+
+    def test_various_names(self):
+        self.assertEqual(generate_soundex("Tymczak"), "T522")
+        self.assertEqual(generate_soundex("Pfister"), "P236")
+        self.assertEqual(generate_soundex("Honeyman"), "H555")
+
+    def test_names_with_h_w(self):
+        self.assertEqual(generate_soundex("Hemming"), "H552")
+        self.assertEqual(generate_soundex("Hemmingway"), "H552")
+
+    def test_zero_padding(self):
+        self.assertEqual(generate_soundex("A"), "A000")
+        self.assertEqual(generate_soundex("Ab"), "A100")
+        self.assertEqual(generate_soundex("Abc"), "A120")
+
 if __name__ == '__main__':
     unittest.main()
